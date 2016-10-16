6e9d466860eb01a16f7a228afefa4080b452c4a6
Tran Khanh Trung <khanhtrung90@yahoo.com>
2016-10-16 12:35:44 +0700

    Update basic features

---
+++ Week01_Flicks/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  Week01_Flicks
+//
+//  Created by Tran Khanh Trung on 10/16/16.
+//  Copyright © 2016 TRUNG. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
